f57047c423191b0a4253ce93b18ed718ec05e21e
Brunoo-Lima <Bruno2.jean.lima@outlook.com>
2025-02-19 21:30:16 -0300

    feat: list replacement added

--- frontend/src/components/layout/admin/list-orders/list-replacement/index.tsx
+++ frontend/src/components/layout/admin/list-orders/list-replacement/index.tsx
@@ -1,3 +1,38 @@
+import { CheckIcon, XIcon } from "lucide-react";
+
 export function ListReplacement() {
-  return <div>Lista de pedidos de troca</div>;
+  return (
+    <div>
+      <table className="w-full">
+        <thead className="text-left p-2">
+          <tr>
+            <th className="w-1/5">Id do pedido</th>
+            <th className="w-1/5">Data do pedido</th>
+            <th className="w-1/5">Valor do pedido</th>
+            <th className="w-1/5">Quantidade de itens</th>
+            <th className="w-1/12 pl-4">Ações</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr className="border-b border-gray-500 h-9">
+            <td>1</td>
+            <td>19/02/2025</td>
+            <td>R$ 500,00</td>
+            <td>2</td>
+            <td className="flex gap-2 ml-2 items-center">
+              <button className="size-7 rounded-full bg-green-500 p-1 flex items-center justify-center">
+                <CheckIcon size={16} color="#ffffff" />
+              </button>
+
+              <button className="size-7 rounded-full bg-error p-1 flex items-center justify-center">
+                <XIcon size={16} color="#ffffff" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
+
