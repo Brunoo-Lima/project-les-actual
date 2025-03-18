d63a1d69f9025e4f74fb5ba0713816a5949d7a5f
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2025-03-18 20:53:08 -0300

    feat: cypress added

--- frontend/src/__tests__/home.test.tsx
+++
@@ -1,9 +0,0 @@
-import Home from "@/app/page";
-import { render, screen } from "@testing-library/react";
-
-describe("Home", () => {
-  it("should render the home page", () => {
-    render(<Home />);
-    // expect(screen.getByText('produtos')).toBeInTheDocument();
-  });
-});
